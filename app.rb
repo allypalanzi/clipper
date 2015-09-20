8fc3a49a8df7b7cc4306fdad289d68be2497c305
Alex Dugger <alexander.dugger@gmail.com>
2015-09-20 13:04:16 -0400

    limit to 30 fps

--- app.rb
+++ app.rb
@@ -17,17 +17,20 @@ class HelloWorldApp < Sinatra::Base
 
   post '/tweet' do
     `ffmpeg -i public/media/bg.mov -i public/media/fore.png \
-    -y -filter_complex "[0:v][1:v] overlay=225:225:enable='between(t,0,20)'" \
+    -y -r 30 -filter_complex "[0:v][1:v] overlay=225:225:enable='between(t,0,20)'" \
     -pix_fmt yuv420p -c:a copy \
     public/media/output.mp4`
 
     # Tweet It
     file = File.new("public/media/output.mp4")
     media_id = JSON.parse(`twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=INIT&media_type=video/mp4&total_bytes=#{file.size}"`)["media_id"]
-    `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=APPEND&media_id=#{media_id}&segment_index=0" --file #{file.path} --file-field "media"`
-    `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=FINALIZE&media_id=#{media_id}"`
-    `twurl "/1.1/statuses/update.json" -d "media_ids=#{media_id}&status=#{params[:message]}"`
 
+    r1 = `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=APPEND&media_id=#{media_id}&segment_index=0" --file #{file.path} --file-field "media"`
+    p r1
+    r2 = `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=FINALIZE&media_id=#{media_id}"`
+    p r2
+    r3= `twurl "/1.1/statuses/update.json" -d "media_ids=#{media_id}&status=#{params[:message]}"`
+    p r3
     send_file 'finished.html'
   end
 
