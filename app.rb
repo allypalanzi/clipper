71e252c29a40aadd969b192ee837790abb3b3e2d
kklai <kkrebeccalai@gmail.com>
2015-09-20 13:26:11 -0400

    the text part

--- app.rb
+++ app.rb
@@ -16,6 +16,8 @@ class HelloWorldApp < Sinatra::Base
   end
 
   post '/tweet' do
+    `phantomjs public/javascripts/capture.js 'HELLLOOOOO' 'PLEASE WORK'`
+
     `ffmpeg -i public/media/bg.mov -i public/media/fore.png \
     -y -r 30 -filter_complex "[0:v][1:v] overlay=225:225:enable='between(t,0,20)'" \
     -pix_fmt yuv420p -c:a copy \
--- public/javascripts/capture.js
+++ public/javascripts/capture.js
@@ -10,12 +10,12 @@ var textcolor = system.args[3]
 if (textcolor == undefined) {
     textcolor = '#bcbcbc'
 }
-console.log("lsdfhlskdjflkds")
-page.content = '<style>.g-text-wrapper {  position: absolute;  width: 700px; height: 300px; padding: 100px 50px 100px 50px;}.g-text-wrapper p {  font-size: 50px;  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;;  font-weight: 100;  color:' + textcolor + ';  opacity: 0.8;  vertical-align: middle;  margin-top:60px;}p.nudge {  font-size: 20px;  letter-spacing: 0.8px;}</style><div class="g-text-wrapper">  <p>' + quote + '</p>  <p class="nudge">— ' + show + '</p></div>';
+
+page.content = '<style>.g-text-wrapper {  position: absolute;  width: 912px; height: 352px; padding: 100px 50px 100px 50px;}.g-text-wrapper p {  font-size: 50px;  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;;  font-weight: 100;  color:' + textcolor + ';  opacity: 0.8;  vertical-align: middle;  margin-top:60px;}p.nudge {  font-size: 20px;  letter-spacing: 0.8px;}</style><div class="g-text-wrapper">  <p>' + quote + '</p>  <p class="nudge">— ' + show + '</p></div>';
 
 page.open(address, function() {
   // being the actual size of the headless browser
-  page.viewportSize = { width: 800, height: 500 };
+  page.viewportSize = { width: 1012, height: 552 };
 
   var clipRect = page.evaluate(function(){
     return document.querySelector('.g-text-wrapper').getBoundingClientRect();
