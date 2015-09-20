2ebd57d16112ad6ad9e6eb22a7154c4c3ea5dc0b
kklai <kkrebeccalai@gmail.com>
2015-09-20 12:33:35 -0400

    write the page from capture.js

--- wave-test/capture.js
+++ wave-test/capture.js
@@ -1,6 +1,14 @@
+//run it like this   phantomjs capture.js INSERTQUOTE INSERTSHOWINFO
+
 var page = require('webpage').create();
 var system = require('system');
-var address = system.args[1]; 
+var address = system.args[1];
+var quote = system.args[1]
+var show = system.args[2]
+
+page.content = '<style>.g-text-wrapper {  position: absolute;  width: 940px;  padding: 100px 50px 100px 50px;}.g-text-wrapper {  position: absolute;  width: 940px; padding: 100px 50px 100px 50px;}.g-text-wrapper p {  font-size: 50px;  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;;  font-weight: 100;  color: #bcbcbc;  opacity: 0.8;  vertical-align: middle;  margin-top:60px;}p.nudge {  font-size: 20px;  letter-spacing: 0.8px;}</style><div class="g-text-wrapper">  <p>' + quote + '</p>  <p class="nudge">— ' + show + '</p></div>';
+
+console.log(page.content)
 
 page.open(address, function() {
   // being the actual size of the headless browser
