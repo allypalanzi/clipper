9e3313361c09bd892f6c9cf52286834533e2fc6e
Alex Dugger <alexander.dugger@gmail.com>
2015-09-20 13:50:08 -0400

    timing audio

--- app.rb
+++ app.rb
@@ -16,15 +16,19 @@ class HelloWorldApp < Sinatra::Base
   end
 
   post '/tweet' do
-    `phantomjs public/javascripts/capture.js 'HELLLOOOOO' 'PLEASE WORK' `
+    `phantomjs public/javascripts/capture.js '#{params["quote"]}' 'made with clpr.' '#ff00ff'`
 
     `ffmpeg -i public/media/bg.mov -i public/media/fore.png \
-    -y -r 30 -filter_complex "[0:v][1:v] overlay=225:225:enable='between(t,0,20)'" \
+    -y -r 30 -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,0,20)'" \
     -pix_fmt yuv420p -c:a copy \
     public/media/output.mp4`
 
-    `ffmpeg -i public/media/output.mp4 -i public/media/cookie-sample.mp3 -ss 0 -t 29 -shortest \
-    -y -c:v copy -c:a aac -strict experimental public/media/out.mp4`
+    # `ffmpeg -i public/media/output.mp4 -i public/media/cookie-sample.mp3 -ss 0 -t 29 -shortest \
+    # -y -c:v copy -c:a aac -strict experimental public/media/out.mp4`
+    length = (params["end-time"].to_f - params["start-time"].to_f).to_i
+
+    `ffmpeg -i public/media/output.mp4 -ss #{params["start-time"].to_i} -t #{length} -i public/media/cookie-sample.mp3\
+    -shortest -y -c:v copy -c:a aac -strict experimental public/media/out.mp4`
 
     # Tweet It
     file = File.new("public/media/out.mp4")
@@ -32,7 +36,7 @@ class HelloWorldApp < Sinatra::Base
 
     `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=APPEND&media_id=#{media_id}&segment_index=0" --file #{file.path} --file-field "media"`
     `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=FINALIZE&media_id=#{media_id}"`
-    `twurl "/1.1/statuses/update.json" -d "media_ids=#{media_id}&status=#{params[:message]}"`
+    `twurl "/1.1/statuses/update.json" -d "media_ids=#{media_id}&status=#{params["message"]}"`
     send_file 'finished.html'
   end
 
