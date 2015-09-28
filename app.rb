4fe0adc57245e82f0f2363ca58e52912370fdcae
Alex Dugger <alexander.dugger@gmail.com>
2015-09-28 11:08:11 -0400

    A mixed bag of tools for making shell calls safer.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'twitter'
 require 'json'
 require 'shellwords'
+require 'open3'
 class HelloWorldApp < Sinatra::Base
   get '/' do
     send_file 'index.html'
@@ -17,8 +18,7 @@ class HelloWorldApp < Sinatra::Base
   end
 
   post '/tweet' do
-
-    `phantomjs public/javascripts/capture.js '#{Shellwords.escape(params["quote"])}' 'made with clpr.' '#ff00ff'`
+    Open3.popen2('phantomjs', 'public/javascripts/capture.js', "#{params["quote"]}", 'made with clpr.', '#000000')
 
     `ffmpeg -i public/media/bg.mov -i public/media/fore.png \
     -y -r 30 -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,0,20)'" \
@@ -32,11 +32,12 @@ class HelloWorldApp < Sinatra::Base
 
     # Tweet It
     file = File.new("public/media/out.mp4")
-    media_id = JSON.parse(`twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=INIT&media_type=video/mp4&total_bytes=#{file.size}"`)["media_id"]
+    media_id = JSON.parse(Open3.popen2('twurl', '-H', 'upload.twitter.com', '/1.1/media/upload.json', '-d', "command=INIT&media_type=video/mp4&total_bytes=#{file.size}")[1].gets)["media_id"]
 
-    `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=APPEND&media_id=#{Shellwords.escape(media_id)}&segment_index=0" --file #{Shellwords.escape(file.path)} --file-field "media"`
-    `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=FINALIZE&media_id=#{Shellwords.escape(media_id)}"`
-    `twurl "/1.1/statuses/update.json" -d "media_ids=#{Shellwords.escape(media_id)}&status=#{Shellwords.escape(params["message"])}"`
+    system('twurl', '-H', 'upload.twitter.com', '/1.1/media/upload.json', '-d', "command=APPEND&media_id=#{media_id}&segment_index=0", '--file', "#{file.path}", '--file-field', 'media')
+    system('twurl', '-H', 'upload.twitter.com', '/1.1/media/upload.json', '-d', "command=FINALIZE&media_id=#{media_id}")
+    system('twurl', "/1.1/statuses/update.json", "-d", "media_ids=#{media_id}&status=#{params["message"]}")
+
     send_file 'finished.html'
   end
 
